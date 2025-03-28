c79bad56b92b132e4e0dcca0f59a3af31712207c
kiabq <conway.joseph@pm.me>
2025-03-28 03:40:31 +0100

    chore: break down core functions and list displays

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,51 @@
 import Foundation
+import ScreenCaptureKit
 
-print("Introduction")
+// Project Requirements:
+/*
+   - Configure screen to be recorded
+   - Init screen recording
+   - Start screen recording
+   - Pause screen recording
+   - Stop screen recording
+*/
 
 func main() {
-    print("Hello, World!")
+    guard #available(macOS 12.3, *) else {
+        print("Invalid macOS version")
+        return
+    }
+    
+    SCShareableContent.getExcludingDesktopWindows(false, onScreenWindowsOnly: false, completionHandler: {content, error in
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
+            return
+        }
+
+        guard let content = content else {
+            print("No shareable content returned.")
+            return
+        }
+
+        print("Displays:")
+        for display in content.displays {
+            print("- \(display.displayID): \(display.width)x\(display.height)")
+        }
+
+        print("Windows:")
+        for window in content.windows {
+            print("- \(window.windowID): \(window.owningApplication?.applicationName ?? "Unknown")")
+        }
+    })
+    
+    RunLoop.main.run(until: Date().addingTimeInterval(10))
 }
+
+
+func initRecording() {}
+
+func startRecording() {}
+
+func stopRecording() {}
 
 main()
